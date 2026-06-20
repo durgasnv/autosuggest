b00c7fea2b1ee28420002d912f1a4fe847085199
DurgaShree <durgashreenag9@gmail.com>
2026-06-20 10:01:32 +0530

    feat: created a toggle card and its functionality

---
+++ frontend/js/toggle.js
@@ -0,0 +1,23 @@
+var users = [
+  {
+    name: "JOHN LEAN",
+    gender: "male",
+    image: "/images/john.png",
+  },
+  {
+    name: "JANE DOE",
+    gender: "female",
+    image: "/images/jane.png",
+  },
+];
+
+var curId = 0;
+
+function toggle() {
+  curId = (curId + 1) % 2;
+
+  var user = users[curId];
+  document.getElementById("user-img").src = user.image;
+  document.getElementById("username").innerText = user.name;
+  document.getElementById("usergender").innerText = user.gender;
+}
